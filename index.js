2c34fa1a8febde1d7ad01dd737c22e1fa28f7a7b
srbm <shawnrbm@gmail.com>
2019-08-12 10:33:00 -0700

    getTeamMatchesData on click and store matches in array.

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-invalid-this */
 'use strict';
 
 $.ajax({
@@ -6,11 +7,35 @@ $.ajax({
   dataType: 'json',
   type: 'GET',
 }).done(function(response) {
-  console.log(response);
+
   for (let i = 0; i<20; i++) {
     const teamId = response.teams[i].id;
-    // teamIds.push(teamId);
+    $('.team:eq('+i+') img').attr('src', response.teams[i].crestUrl);
     $('.team:eq('+i+')').data('team-id', teamId);
-    console.log($('.team:eq('+i+')').data('team-id'));
+    $('.team:eq('+i+')').append(`<h3>${response.teams[i].name}</h3>`)
   }
 });
+
+// eslint-disable-next-line require-jsdoc
+function getTeamMatchesData() {
+  $('.team').on('click', function() {
+    const teamMatches = [];
+    const teamId = $(this).data('team-id');
+    console.log(teamId);
+    $.ajax({
+      headers: {'X-Auth-Token': footballToken},
+      url: `http://api.football-data.org/v2/teams/${teamId}/matches?dateFrom=2018-08-10&dateTo=2019-05-12`,
+      dataType: 'json',
+      type: 'GET',
+    }).done(function(response) {
+      for (let i = 0; i<response.count; i++) {
+        if (response.matches[i].competition.name === "Premier League") {
+          teamMatches.push(response.matches[i]);
+        }
+      }
+      console.log(teamMatches);
+    });
+  });
+}
+
+$(getTeamMatchesData);
